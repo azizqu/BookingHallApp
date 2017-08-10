b5382838ecd46600778ee16c11ca919560525559
Aziz <azizqu@gmail.com>
2017-08-10 19:41:19 -0400

    Added Client Class

---
+++ BookingHallAppDraft/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingHallAppDraft.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
