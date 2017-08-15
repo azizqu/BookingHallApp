4e11d33f5419bf1bd77d9f3e7016efc03d83badd
Aziz <azizqu@gmail.com>
2017-08-14 21:06:17 -0400

    Some formatting changes

--- BookingHallAppDraft/Models/Reservations.cs
+++ BookingHallAppDraft/Models/Reservations.cs
@@ -9,13 +9,15 @@ namespace BookingHallAppDraft.Models
     public class Reservations
     {
         public int ReservationId { get; set; }
-
+        [Required]
+        [Display(Name="Client ID")]
         public int ClientId { get; set; }
             public string ClientName { get; set; }
-
+        [Required]
+        [Display(Name = "Hall ID")]
         public int HallId { get; set; }
             public string HallName { get; set; }
-
+        [Required]
         [DisplayFormat (DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set;}
 
